caa7bc6194955ec045ab17214a6cf59d407fa6fc
MPettersen <mp.markus94@gmail.com>
2019-08-23 11:29:21 +0200

    fixed errors

--- assignment1/src/main/java/assignment1/Fibonacci.java
+++ assignment1/src/main/java/assignment1/Fibonacci.java
@@ -1,11 +1,17 @@
 package assignment1;
 
 public class Fibonacci {
-	   // Exercise 1: Fibonacci numbers
-	   //
-	   // fibonacci(n) returns nth Fibonacci number, and is defined by the
-	   // recurrence relation F_n = F_n-1 + F_n-2, with seed values F_0=0 and F_1=1.
+  // Exercise 1: Fibonacci numbers
+  //
+  // fibonacci(n) returns nth Fibonacci number, and is defined by the
+  // recurrence relation F_n = F_n-1 + F_n-2, with seed values F_0=0 and F_1=1.
 	public long getFibonacci(long number) {
-		return 0;
+		if (number <= 0) {
+			return 0;
+		}
+		if (number == 1) {
+		  return 1;
+		}
+		return getFibonacci(number-1) + getFibonacci(number-2);
 	}
 }
