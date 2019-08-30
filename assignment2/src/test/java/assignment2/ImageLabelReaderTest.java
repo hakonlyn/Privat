f08304981533f4dde8206903b71a71b464a5380a
Vinay Setty <vsetty@k122-237.ux.uis.no>
2019-08-30 14:32:29 +0200

    added imaglabelread

---
+++ assignment2/src/test/java/assignment2/ImageLabelReaderTest.java
@@ -0,0 +1,22 @@
+package assignment2;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+public class ImageLabelReaderTest {
+	 @Test public void testLabelFiles() {
+		 ImageLabelReader ilr = new ImageLabelReader("data/label_mapping.csv", "data/image_mapping.csv");
+		 String[] files = ilr.getImageFiles("goldfish");
+		 assertNotNull(files);
+		 assertEquals(files.length, 10);		 
+	 }
+	 
+	 @Test public void testImageLabel() {
+		 ImageLabelReader ilr = new ImageLabelReader("data/label_mapping.csv", "data/image_mapping.csv");
+		 String label = ilr.getLabel("ILSVRC2012_val_00000090.JPEG");
+		 assertNotNull(label);
+		 assertEquals(label, "American alligator");		 
+	 }
+}
