8a81ddff1007b7a1da9e3f984faddbc10e086a7d
Vemund Refnin <vemund.refnin@gmail.com>
2019-09-05 14:54:54 +0200

    merge

--- assignment2/src/main/java/assignment2/ImageLabelReader.java
+++ assignment2/src/main/java/assignment2/ImageLabelReader.java
@@ -4,12 +4,10 @@ import java.io.*;
 import java.util.*;
 
 import javax.print.event.PrintJobListener;
-import com.google.common.collect.BiMap;
 
 
 public class ImageLabelReader {
-	// BidiMap<String, String> imageMap = new DualHashBidiMap<>();
-	BiMap<String, String> imageMap = HashBiMap.create();
+	HashMap<String, String> imageMap = new HashMap<String, String>();
 	public static void main(String[]args){
 
 	}
@@ -20,28 +18,21 @@ public class ImageLabelReader {
 			Scanner imageScanner = new Scanner(new File(imageMappingFile));
 
 			while (imageScanner.hasNextLine()){
-				Array imageLine =imageScanner.nextLine().split(" ");
+				String[] imageLine =imageScanner.nextLine().split(" ");
 				String image = imageLine[0];
 				String imagefileLable = imageLine[1];
-				// System.out.println(line);
-				// System.out.println(line.split(" ")[0]);
-				// System.out.println();
 				while (lableScanner.hasNextLine()){
-					Array lableLine =lableScanner.nextLine().split(" ");
+					String[] lableLine =lableScanner.nextLine().split(" ");
 					String lableNumber = lableLine[0];
 					String lable = lableLine[1];
-
 					if (imagefileLable == lableNumber){
 						this.imageMap.put(image, lable);
 					}
-
-					// System.out.println(line);
-					// System.out.println(line.split(" ")[0]);
-					// System.out.println(line.split(" ")[1]);
 				}
-				imageScanner.close();
+				lableScanner.close();
 			}
 			imageScanner.close();
+			System.out.println(this.imageMap);
 
 			
 
@@ -55,7 +46,8 @@ public class ImageLabelReader {
 	//Note that each image has a single label
 	//If the given image file is not found return null
 	public String getLabel(String imageFileName){
-		return this.imageMap.getKey(imageFileName);
+		System.out.println(this.imageMap.get(imageFileName));
+		return this.imageMap.get(imageFileName);
 	}
 	
 	//Given a label get all the image files corresponding to this label
