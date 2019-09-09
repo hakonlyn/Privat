18b2ec21f73fc1b164a120f795174f7dc8fa3074
Vemund Refnin <vemund.refnin@dinerp.no>
2019-09-09 15:32:17 +0200

    first part successful on local tests

--- assignment2/src/test/java/assignment2/ImageLabelReaderTest.java
+++ assignment2/src/test/java/assignment2/ImageLabelReaderTest.java
@@ -6,17 +6,20 @@ import static org.junit.Assert.assertNotNull;
 import org.junit.Test;
 
 public class ImageLabelReaderTest {
-	 @Test public void testLabelFiles() {
-		 ImageLabelReader ilr = new ImageLabelReader("data/label_mapping.csv", "data/image_mapping.csv");
-		 String[] files = ilr.getImageFiles("goldfish");
-		 assertNotNull(files);
-		//  assertEquals(files.length, 10);		 
-	 }
-	 
-	//  @Test public void testImageLabel() {
-	// 	 ImageLabelReader ilr = new ImageLabelReader("data/label_mapping.csv", "data/image_mapping.csv");
-	// 	 String label = ilr.getLabel("ILSVRC2012_val_00000090.JPEG");
-	// 	 assertNotNull(label);
-	// 	 assertEquals(label, "American alligator");		 
-	//  }
+
+	@Test
+	public void testLabelFiles() {
+		ImageLabelReader ilr = new ImageLabelReader("data/label_mapping.csv", "data/image_mapping.csv");
+		String[] files = ilr.getImageFiles("goldfish");
+		assertNotNull(files);
+		assertEquals(files.length, 10);
+	}
+
+	@Test
+	public void testImageLabel() {
+		ImageLabelReader ilr = new ImageLabelReader("data/label_mapping.csv", "data/image_mapping.csv");
+		String label = ilr.getLabel("ILSVRC2012_val_00000090.JPEG");
+		assertNotNull(label);
+		assertEquals(label, "American alligator");
+	}
 }
